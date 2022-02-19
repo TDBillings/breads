3e6aa40554472c37be67319956984a9efecb682a
William Sullivan <sullivanwp95@gmail.com>
2022-02-19 01:15:11 -0500

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -30,6 +30,14 @@ router.post('/', (req, res) => {
     res.redirect('/breads')
 })
 
+// EDIT
+router.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray
+    })
+})
+
 // Show
 router.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -48,5 +56,16 @@ router.delete('/:indexArray', (req, res) => {
     Bread.splice(req.params.indexArray, 1)
     res.status(303).redirect('/breads')
   })
+
+// UPDATE
+router.put('/:arrayIndex', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten === true
+    } else {
+        req.body.hasGluten === false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+})
 
 module.exports = router
--- views/show.jsx
+++ views/show.jsx
@@ -17,6 +17,8 @@ function Show({bread, index}) {
             </p>
             <img src={bread.image} alt={bread.name} id={bread.name}></img>
             <button><a href='/breads'>Go Home</a></button>
+            <br></br>
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE"/>
             </form>
